2765d1a37343f20a0dba02bc8ed324d334e16e63
Monika <133509111+Monikagith@users.noreply.github.com>
2026-07-14 12:39:03 +0530

    Update hooks.py

--- hooks.py
+++ hooks.py
@@ -1,3 +1,14 @@
-app_include_css = "/assets/swayatt_theme/css/custom.css"
+app_name = "swayatt_theme"
+app_title = "Swayatt Theme"
+app_publisher = "Swayatt"
+app_description = "Custom Theme for ERPNext"
+app_email = "sunny@swayatt.com"
+app_license = "MIT"
 
-app_include_js = "/assets/swayatt_theme/js/custom.js"
+app_include_css = [
+    "/assets/swayatt_theme/css/custom.css",
+]
+
+app_include_js = [
+    "/assets/swayatt_theme/js/custom.js",
+]
